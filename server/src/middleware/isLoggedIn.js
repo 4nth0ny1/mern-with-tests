c559d3bc7ce5bb34551735fc6bf920558230b8c3
4nth0ny1 <acatullo4@gmail.com>
2023-08-15 18:32:06 -0500

    now getting todos route successfully

--- server/src/middleware/isLoggedIn.js
+++ server/src/middleware/isLoggedIn.js
@@ -1,9 +1,18 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    res.status(401).send("invalid credentials");
+    res.status(401).send("invalid credentials"); // 401: wrong token
   } else {
     const token = authHeader.split(" ")[1];
     console.log(token);
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        res.status(403).send("invalid credentials"); // 403: bad token.like hacker
+      } else {
+        next();
+      }
+    });
   }
 };
