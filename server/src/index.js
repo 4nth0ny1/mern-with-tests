365d0744ee323dba7c176c8da2ff1d6782aee8c4
4nth0ny1 <acatullo4@gmail.com>
2023-08-15 18:15:37 -0500

    added cors and morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
 
 dotenv.config();
 console.log(process.env.MONGODB_URI);
 
 // setup express server
 const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
 
 app.get("/todos", (req, res) => {
   res.send("hello anthony");
