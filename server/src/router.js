eb07089a34cc1d35233b4c935e6b883df8f5b171
4nth0ny1 <acatullo4@gmail.com>
2023-08-15 18:27:43 -0500

    added todosRoute

--- server/src/router.js
+++ server/src/router.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 const loginRoute = require("./routes/loginRoute");
+const todosRoute = require("./routes/todosRoute");
 
 router.post("/login", loginRoute);
+router.get("/todos", todosRoute);
 
 module.exports = router;
---
+++ server/src/routes/todosRoute.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.send("todosRoute is a success");
+};
