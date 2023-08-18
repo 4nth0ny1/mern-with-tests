abc2392783aa3ed7c07d00aad1780e3d7d16e75f
4nth0ny1 <acatullo4@gmail.com>
2023-08-17 22:37:39 -0500

    working axios get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
-// import "./App.css";
-import React from "react";
-import getTodosRequest from "./api/getTodosRequest";
+import React, { useState, useEffect } from "react";
+// import { getTodos } from "./api/getTodosRequest";
+import axios from "axios";
 
 function App() {
-  const todos = getTodosRequest();
-  console.log(todos);
+  const [todos, setTodos] = useState([]);
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5MjE0MTg4NX0.Lor7yvj-KMwgk69L8Rcf93W02L430m8GGtV5ue85GkY";
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("http://localhost:8080/todos", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          setTodos(response.data);
+        });
+    }, 3000);
+  }, []);
 
   return (
     <div className="App">
       <h1>Mern Todo App</h1>
-      {/* {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            {todo.text}: {todo.completed ? "Complete" : "Not Complete"}
-          </div>
-        );
-      })} */}
+      {todos.length === 0 ? (
+        <div>Loading ...</div>
+      ) : (
+        todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              {todo.text}: {todo.completed ? "Complete" : "Not Complete"}
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
